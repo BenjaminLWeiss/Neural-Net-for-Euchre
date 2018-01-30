224ca7fd3622abc6952390ead689a49f41200258
Aubrey <aubrey>
2018-01-29 20:40:21 -0700

    Loads checkpointed models at startup

--- train.py
+++ train.py
@@ -1,12 +1,16 @@
-def train(numEpochs) :
+def train(numEpochs,filenames) :
 	from computerPlayer import ComputerPlayer
 	from euchreGameMaster import GameMaster
 	from euchreGameMaster import POSITIONS
 
 	p1 = ComputerPlayer()
+	p1.load(filenames[0])
 	p2 = ComputerPlayer()
+	p2.load(filenames[1])
 	p3 = ComputerPlayer()
+	p3.load(filenames[2])
 	p4 = ComputerPlayer()
+	p4.load(filenames[3])
 
 	game = GameMaster(p1,p2,p3,p4)
 
@@ -27,11 +31,11 @@ def train(numEpochs) :
 		
 		print "Played %d hands" % handCount
 
-		p1.save('p1.mdl')
-		p2.save('p2.mdl')
-		p3.save('p3.mdl')
-		p4.save('p4.mdl')
+		p1.save(filenames[0])
+		p2.save(filenames[1])
+		p3.save(filenames[2])
+		p4.save(filenames[3])
 
 if __name__ == "__main__" :
-	train(-1)
+	train(-1,['p1.mdl','p2.mdl','p3.mdl','p4.mdl'])
 
